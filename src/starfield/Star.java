75c7cfe0550876ec77298e6476d5f9d192547b30
Ambesh Tiwari <35375583+ambesh123@users.noreply.github.com>
2021-09-26 13:56:08 +0530

    Improved Star.java

--- src/starfield/Star.java
+++ src/starfield/Star.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package starfield;
+import java.awt.Color;
 import java.awt.Graphics;
 /**
  *
@@ -11,12 +12,14 @@ import java.awt.Graphics;
  */
 public class Star {
     int x,y,z,r,pz;
+    Color color;
     Star()
     {   
         
         x= (int)((2*Math.random()-1)*StarField.frame.getWidth())*500;
         y= (int)((2*Math.random()-1)*StarField.frame.getHeight())*500;
         z=(int)(Math.random()*500); pz=z;
+        color = Color.getHSBColor((float)Math.random(), (float)Math.random(), (float)Math.random());
     }
     void update(){
         pz=z;
@@ -24,13 +27,14 @@ public class Star {
         if(z<10){
              x= (int)((2*Math.random()-1)*StarField.frame.getWidth())*500;
              y= (int)((2*Math.random()-1)*StarField.frame.getHeight())*500;
-            z=500;  pz=500;
+             z=500;  pz=500;
+             color = Color.getHSBColor((float)Math.random(), (float)Math.random(), (float)Math.random());
         }
     }
     
     void show(Graphics g){
         r=1000/z;
         g.fillOval(x/z, y/z, r, r);
-        g.drawLine(x/z,y/z,x/pz,y/pz);
+        g.drawLine(x/z+r/2, y/z+r/2, x/pz+r/2, y/pz+r/2);
     }
 }
